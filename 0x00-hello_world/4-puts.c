cf234285a61c88c9813ac6d348a1fae36da6861e
Jeremy Ola <34716486+xXJayDoeXx@users.noreply.github.com>
2021-06-17 11:43:36 +0100

    documented script 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,10 @@
 #include <stdio.h>
 
+/**
+ * main - starting point of the program
+ * 
+ * Return - 0 if program executes properly, else a non-zero number
+ */
 int main()
 {
 	puts("\"Programming is like building a multilingual puzzle");
