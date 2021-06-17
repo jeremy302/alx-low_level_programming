2b16aad483837ef354eab8f890393a32f0f25700
Jeremy Ola <34716486+xXJayDoeXx@users.noreply.github.com>
2021-06-17 14:15:48 +0100

    used fwrite in 101-quote

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -8,9 +8,9 @@
  */
 int main(void)
 {
-	fputs(
-		"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",
-		stderr
-		);
+	char err_msg[] =
+		"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+	fwrite(err_msg, 1, sizeof(err_msg) - 1, stderr);
+
 	return (1);
 }
