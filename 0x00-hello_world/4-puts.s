1852ccacdf4a09cfda7753a7844e76f91187f266
Jeremy Ola <34716486+xXJayDoeXx@users.noreply.github.com>
2021-06-17 13:16:13 +0100

    testing intel asm syntax with gcc

---
+++ 0x00-hello_world/4-puts.s
@@ -0,0 +1,29 @@
+	.file	"4-puts.c"
+	.text
+	.def	__main;	.scl	2;	.type	32;	.endef
+	.section .rdata,"dr"
+	.align 8
+.LC0:
+	.ascii "\"Programming is like building a multilingual puzzle\0"
+	.text
+	.globl	main
+	.def	main;	.scl	2;	.type	32;	.endef
+	.seh_proc	main
+main:
+	pushq	%rbp
+	.seh_pushreg	%rbp
+	movq	%rsp, %rbp
+	.seh_setframe	%rbp, 0
+	subq	$32, %rsp
+	.seh_stackalloc	32
+	.seh_endprologue
+	call	__main
+	leaq	.LC0(%rip), %rcx
+	call	puts
+	movl	$0, %eax
+	addq	$32, %rsp
+	popq	%rbp
+	ret
+	.seh_endproc
+	.ident	"GCC: (Rev10, Built by MSYS2 project) 10.2.0"
+	.def	puts;	.scl	2;	.type	32;	.endef
