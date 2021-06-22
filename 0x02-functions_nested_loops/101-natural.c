e524861265264f4b4ba8cfe886cd8325299c81fd
jeremy302 <olajeremy123@gmail.com>
2021-06-22 22:13:51 +0100

    testing

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -1,4 +1,5 @@
 #include <stdlib.h>
+#include <stdio.h>
 #include "holberton.h"
 
 void putstr(char *str);
@@ -11,7 +12,6 @@ void putstr(char *str);
 int main(void)
 {
 	int i, sum;
-	char sum_str[10];
 
 	sum = 0;
 	for (i = 0; i < 1024; i++)
@@ -19,20 +19,7 @@ int main(void)
 		if (i % 3 == 0 || i % 5 == 0)
 			sum += i;
 	}
-	itoa(sum, sum_str, 10);
-	putstr(sum_str);
+	printf("%d", sum);
 
 	return (0);
-}
-
-/**
- * putstr - writes a char array to stdout till it sees the null terminator: \0
- * @str: Pointer to the first array element
- *
- * Return: void
- */
-void putstr(char *str)
-{
-	while (*str != '\0')
-		_putchar(*str++);
 }
