2f4ab9f0079c632092dadae665ca1540ad8b184f
unknown <jeremy302>
2021-06-22 20:13:13 +0100

    testing holberton.h

--- 0x02-functions_nested_loops/holberton.h
+++ 0x02-functions_nested_loops/holberton.h
@@ -1,43 +1,14 @@
-//#include <unistd.h>
 
-
-/**
- * _putchar - writes the character c to stdout
- * @c: The character to print
- *
- * Return: On success 1.
- * On error, -1 is returned, and errno is set appropriately.
- */
 int _putchar(char);
-
-/**
- * putstr - writes a char array to stdout till it sees the null terminator: \0
- * @str: Pointer to the first array element
- *
- * Return: void
- */
-void putstr(char *str); //main.c
-
-void print_alphabet(void); //1-alphabet.c
-
-void print_alphabet_x10(void); //2-print_alphabet_x10.c
-
-int _islower(int c); //3-islower.c
-
-int _isalpha(int c); //4-isalpha.c
-
-int print_sign(int n); //5-sign.c
-
-int _abs(int); //6-abs.c
-
-int print_last_digit(int); //7-print_last_digit.c
-
-void jack_bauer(void); //8-24_hours.c
-
-void times_table(void); //9-times_table.c
-
-int add(int, int); //10-add.c
-
-void print_to_98(int n); //11-print_to_98.c
-
-void print_times_table(int n); //100-times_table.c
+void print_alphabet(void);
+void print_alphabet_x10(void);
+int _islower(int c);
+int _isalpha(int c);
+int print_sign(int n);
+int _abs(int);
+int print_last_digit(int);
+void jack_bauer(void);
+void times_table(void);
+int add(int, int);
+void print_to_98(int n);
+void print_times_table(int n);
