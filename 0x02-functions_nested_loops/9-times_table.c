155727de243e7b4fcabb0c2e2d8c5bbae53b8273
jeremy302 <olajeremy123@gmail.com>
2021-06-22 21:07:54 +0100

    testing scripts

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -7,22 +7,19 @@
  */
 void times_table(void)
 {
-	int i, j, product, product_first_digit, 
-		product_second_digit, product_last_digit;
+	int i, j, product, product_first_digit, product_last_digit;
 
 	for (i = 0; i <= 9; i++)
 	{
 		for (j = 0; j <= 9; j++)
 		{
 			product = i * j;
-			product_first_digit = product / 100;
-			product_second_digit = (product / 10) % 10;
+			product_first_digit = product / 10;
 			product_last_digit = product % 10;
 
 			if (j != 0)
 			{
 				_putchar(product_first_digit == 0 ? ' ' : product_first_digit + '0');
-				_putchar(product_second_digit == 0 ? ' ' : product_second_digit + '0');
 			}
 			_putchar(product_last_digit + '0');
 
