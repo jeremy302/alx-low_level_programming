7c842b11619bec5e2abbf4a10a4a2f094acb5226
jeremy302 <olajeremy123@gmail.com>
2021-06-24 13:02:50 +0100

    testing

--- 0x03-debugging/3-print_remaining_days.c
+++ 0x03-debugging/3-print_remaining_days.c
@@ -24,7 +24,12 @@ void print_remaining_days(int month, int day, int year)
 	}
 	else
 	{
-		if (month == 2 && day == 60)
+		if ((month == 2 && day == 60) || (month == 3 && day == 91)
+			 || (month == 4 && day == 121) || (month == 5 && day == 152)
+			 || (month == 6 && day == 182) || (month == 7 && day == 213)
+			 || (month == 8 && day == 244) || (month == 9 && day == 274)
+			 || (month == 10 && day == 305) || (month == 11 && day == 335)
+			 || (month == 12 && day == 366))
 		{
 			printf("Invalid date: %02d/%02d/%04d\n", month, day - 31, year);
 		}
