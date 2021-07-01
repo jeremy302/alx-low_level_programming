12d6e2dff4f55b29613e21a22184c5b94e566438
jeremy302 <olajeremy123@gmail.com>
2021-07-01 21:09:37 +0100

    patch

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -15,7 +15,9 @@ char *_strncpy(char *dest, char *src, int n)
 	while (n && (*dest++ = *src++))
 		--n;
 	while (n-- > 0)
-		*dest++ = 0;
+		*(dest++ - 1) = 0;
+	if (n == -1 && *(dest - 1) != '\0')
+		*dest = '\0';
 
 	return (dest_tmp);
 }
