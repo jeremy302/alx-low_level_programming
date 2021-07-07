d1f27176ecb83cb9ae625c33dc82165e67ab3e95
jeremy302 <olajeremy123@gmail.com>
2021-07-07 21:23:45 +0100

    patch

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -14,7 +14,7 @@ int wildcmp(char *s1, char *s2)
 		if (*s1 == '\0' || *(s2 + 1) == '*')
 			return (wildcmp(s1, s2 + 1));
 		else if (wildcmp(s1, s2 + 1))
-			return (wildcmp(s1 + 1, s2 + 2));
+			return true;
 		return (wildcmp(s1 + 1, s2));
 	}
 	else if (*s2 == '\0')
