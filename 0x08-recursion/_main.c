e791bb895212636239ab347822beb8c9590befb0
jeremy302 <olajeremy123@gmail.com>
2021-07-09 11:25:40 +0100

    added 0x09

--- 0x08-recursion/_main.c
+++ 0x08-recursion/_main.c
@@ -12,7 +12,7 @@ int main(void)
 {
 	int r;
 
-    r = wildcmp("mainck.c", "ma**c");
+    r = wildcmp("main.c", "ma********************************c");
 	printf("%d\n", r);
 //	return 0;
 	r = wildcmp("main.c", "*.c");
