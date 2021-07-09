6642e4689d05d77ff759a3f4f40a59286c152ac5
jeremy302 <olajeremy123@gmail.com>
2021-07-09 15:11:40 +0100

    patch

---
+++ 0x09-static_libraries/holberton_files/2-strncpy.c
@@ -0,0 +1,21 @@
+#include "holberton.h"
+
+/**
+ * _strncpy - copies  src to dest
+ * @dest: array to overwrite
+ * @src: array to copy from
+ * @n: max bytes to copy
+ *
+ * Return: dest
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+	char *dest_tmp = dest;
+
+	while (n && (*dest++ = *src++))
+		--n;
+	while (n-- > 0)
+		*(dest++ - 1) = 0;
+
+	return (dest_tmp);
+}
