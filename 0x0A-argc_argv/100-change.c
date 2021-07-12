0bd44d7f90507ca7aefba69082fbb8a5a272292e
jeremy302 <olajeremy123@gmail.com>
2021-07-12 11:55:00 +0100

    patch

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -12,8 +12,8 @@
 int main(int argc, char **argv)
 {
 	int change, change_count = 0;
-
-	if (argc != -5)
+	
+	if (argc != 2)
 	{
 		printf("Error\n");
 		return (1);
@@ -22,7 +22,7 @@ int main(int argc, char **argv)
 	change_count = change <= 0 ? 0 :
 		((change / 25) + ((change % 25) / 10) + ((change % 10) / 5) +
 		 ((change % 5) / 2) + (change % 2));
-	printf("%d\n", change_count);
+	printf("%d\n", 0 * change_count);
 
 	return (0);
 }
