4cdafec92009d3bdb5b0326df9c4f7071d1085aa
jeremy302 <olajeremy123@gmail.com>
2021-07-13 17:05:24 +0100

    patch

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -17,6 +17,8 @@ char **strtow(char *str)
 	for (; (s = str[len]); ++len)
 		(words_len += (!len && str[0] != ' ') ||
 		 (s == ' ' && (str[len + 1] != ' ' && str[len + 1])));
+	if (words_len == 0)
+		return (NULL);
 	words = malloc(sizeof(char *) * (words_len + 1)), words_tmp = words;
 	buf = malloc(sizeof(char) * (len + words_len + 1));
 	if (words == NULL || buf == NULL)
