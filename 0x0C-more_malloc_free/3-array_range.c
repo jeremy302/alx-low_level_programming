cb3996083df2a2c307b9d0a136f4b665598a4b46
jeremy302 <olajeremy123@gmail.com>
2021-07-15 18:41:43 +0100

    added 0x0C

--- 0x0C-more_malloc_free/3-array_range.c
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -12,14 +12,14 @@ int *array_range(int min, int max)
 {
 	int *ptr, *ptr_tmp;
 
-	if (max < min || max - min > 10000)
+	if (max < min)
 		return (NULL);
 
 	ptr = malloc(sizeof(int) * (max - min + 1)), ptr_tmp = ptr;
 	if (ptr == NULL)
 		return (NULL);
 	while (min <= max)
-		*ptr++ = min++;
+		*ptr = min++, ptr += min < max;
 
 	return (ptr_tmp);
 }
