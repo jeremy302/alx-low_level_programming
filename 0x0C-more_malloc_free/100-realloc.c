e2be6b3c1344c3a0a74eff935ac69a8e67761475
jeremy302 <olajeremy123@gmail.com>
2021-07-16 01:03:07 +0100

    patch

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -27,7 +27,7 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 	ptr_ = malloc(new_size);
 	if (ptr_ == NULL)
 		return (free(ptr), NULL);
-	for (; i <= old_size && i <= new_size; i++)
+	for (; i < old_size && i < new_size; i++)
 		ptr_[i] = ((char *)ptr)[i];
 	free(ptr);
 	return (ptr_);
