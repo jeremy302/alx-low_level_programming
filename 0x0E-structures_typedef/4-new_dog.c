1cc61e98a6550fdd0c0b45a75e662563687f7500
jeremy302 <olajeremy123@gmail.com>
2021-07-19 16:47:41 +0100

    patch

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -19,6 +19,8 @@ dog_t *new_dog(char *name, float age, char *owner)
 		name = "";
 	if (owner == NULL)
 		owner = "";
+	if (age <= 0)
+		return (NULL);
 	while (name != NULL && name[name_len])
 		name_len++;
 	while (owner != NULL && owner[owner_len])
--- 0x0E-structures_typedef/4_main.c
+++ 0x0E-structures_typedef/4_main.c
@@ -13,6 +13,6 @@ int main(void)
 	char *name = NULL;
     my_dog = new_dog(name, 3.5, "Bob");
 	/*name[1] = 'k';*/
-    printf("My name is %s, and I am %.1f :) - Woof!\n", my_dog->name, my_dog->age);
+    printf("My name is |%s| (|%s|%s|), and I am %.1f :) - Woof!\n", my_dog->name, name, my_dog->owner, my_dog->age);
     return (0);
 }
