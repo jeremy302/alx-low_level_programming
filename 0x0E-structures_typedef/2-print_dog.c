784c72bcfa8473f3ee94d8866f90bbe53c577104
jeremy302 <olajeremy123@gmail.com>
2021-07-19 11:04:36 +0100

    patch

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -3,15 +3,15 @@
 
 /**
  * print_dog - prints a dog's name, age and owner
- * @d - dog to print
+ * @d: dog to print
  *
  * Return: void
  */
 void print_dog(struct dog *d)
 {
-	char *name = d->name == NULL ? "(nil)" : d->name;
-	char *owner = d->owner == NULL ? "(nil)" : d->owner;
-	
+	char *name = d == NULL || d->name == NULL ? "(nil)" : d->name;
+	char *owner = d == NULL || d->owner == NULL ? "(nil)" : d->owner;
+
 	if (d != NULL)
 		printf("%s\n%f\n%s\n", name, d->age, owner);
 }
