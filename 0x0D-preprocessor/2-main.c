64442b786a234b3afd32148d4054b1525335a7f7
jeremy302 <olajeremy123@gmail.com>
2021-07-19 16:30:10 +0100

    patch

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,7 +1,12 @@
 #include <stdio.h>
 
+/**
+ * main - prints the file it was compiled with
+ *
+ * Return: 0
+ */
 int main(void)
 {
-    printf("%s", __FILE__);
-    return (0);
+	printf("%s", __FILE__);
+	return (0);
 }
