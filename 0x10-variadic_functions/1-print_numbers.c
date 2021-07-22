3984555498213c06f99eac5d046b6b1ddad7f8dc
jeremy302 <olajeremy123@gmail.com>
2021-07-22 19:25:27 +0100

    patch

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -18,5 +18,7 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 	while (i++ < n)
 		printf("%i%s", va_arg(args, int),
 			   i == n ? "\n" : separator == NULL ? "" : separator);
+	if (n == 0)
+		printf("\n");
 	va_end(args);
 }
