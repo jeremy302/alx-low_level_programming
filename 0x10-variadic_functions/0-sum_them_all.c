01ea1d19578a9040bce0a94a418e6add06a78277
jeremy302 <olajeremy123@gmail.com>
2021-07-22 21:36:39 +0100

    patch

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -15,7 +15,7 @@ int sum_them_all(const unsigned int n, ...)
 
 	va_start(args, n);
 	while (i++ < n)
-		sum += va_arg(args, unsigned int);
+		sum += va_arg(args, int);
 	va_end(args);
 	return (sum);
 }
