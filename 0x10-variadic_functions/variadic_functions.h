3dbb4aa8865cfa304bc7f0ad75d9b37fe561ecc1
jeremy302 <olajeremy123@gmail.com>
2021-07-22 19:20:35 +0100

    patch

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -1,5 +1,8 @@
 #ifndef VARIADIC_H
 #define VARIADIC_H
+#ifndef NULL
+#define NULL ((void *)0)
+#endif
 
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
