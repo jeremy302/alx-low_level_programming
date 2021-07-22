1c1c96a24b2ad078dedb20e3db74b388c058b5a0
jeremy302 <olajeremy123@gmail.com>
2021-07-22 20:21:18 +0100

    patch

--- 0x10-variadic_functions/100-hello_holberton.asm
+++ 0x10-variadic_functions/100-hello_holberton.asm
@@ -1,13 +1,16 @@
-global    _start
-section   .text
-_start:
-mov       rax, 1
-mov       rdi, 1
-mov       rsi, message
-mov       rdx, 17
-syscall
-mov       rax, 60
-xor       rdi, rdi
-syscall
-section   .data
-message:  db        "Hello, Holberton", 10
+	global main
+	section   .text
+main:
+	mov       rax, 1 				; write
+	mov       rdi, 1				; stdout
+	mov       rsi, str
+	mov       rdx, 17				; str length
+	syscall
+	mov       rax, 60				; exit
+	xor       rdi, rdi				; 0 status
+	syscall
+
+
+	section   .data
+str:
+	db        "Hello, Holberton", 10 ; 10 == '\n'
