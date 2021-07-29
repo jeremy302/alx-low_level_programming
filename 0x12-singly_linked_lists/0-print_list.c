6414e76a422f70e81f598d5efa99dd76880e4284
jeremy302 <olajeremy123@gmail.com>
2021-07-29 18:19:42 +0100

    patch

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -11,9 +11,7 @@ size_t print_list(const list_t *h)
 {
 	size_t len = 0, str_len = 0;
 
-	if (h == NULL)
-		return (-1);
-	for (; h != NULL; h = h->next, len++)
+	for (; h != NULL; h = h->next, len++, str_len = 0)
 	{
 		if (h->str != NULL)
 			while (h->str[str_len])
