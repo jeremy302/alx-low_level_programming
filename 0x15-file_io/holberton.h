af021b058134bdb8221d36e42826767280d496a5
jeremy302 <olajeremy123@gmail.com>
2021-08-09 18:57:39 +0100

    added 0x05 holberton.h header

---
+++ 0x15-file_io/holberton.h
@@ -0,0 +1,24 @@
+
+#ifndef MAIN_H
+#define MAIN_H
+#include <stdlib.h>
+#include <elf.h>
+
+int _putchar(char c);
+ssize_t read_textfile(const char *filename, size_t letters);
+int create_file(const char *filename, char *text_content);
+int append_text_to_file(const char *filename, char *text_content);
+
+void print_elf_header(Elf64_Ehdr *h);
+void print_elf_header_2(Elf64_Ehdr *h);
+void put_err(char *str);
+
+#endif /* MAIN_H */
+
+#ifndef HEADERS_H
+#define HEADERS_H
+#include <unistd.h>
+#include <fcntl.h>
+#include <stdio.h>
+#undef HEADERS_H
+#endif /* HEADERS_H */
