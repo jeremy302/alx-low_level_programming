517ce6e220e12862fa549f38e69079956bf3318c
jeremy302 <olajeremy123@gmail.com>
2021-09-16 22:49:35 +0100

    patch

--- 0x17-doubly_linked_lists/3-add_dnodeint_end.c
+++ 0x17-doubly_linked_lists/3-add_dnodeint_end.c
@@ -19,7 +19,6 @@ dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
 	current = *head;
 	while (current->next != NULL)
 		current = current->next;
-	if (*head != node)
-		current->next = node, node->prev = current;
+	current->next = node, node->prev = current;
 	return (node);
 }
