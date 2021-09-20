c23c8c5f5edf3aeff80cbb73fd7fb56290db97b5
jeremy302 <olajeremy123@gmail.com>
2021-09-20 22:27:59 +0100

    patch

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget https://github.com/jeremy302/alx-low_level_programming/raw/main/0x18-dynamic_libraries/100-winner.so -O "$HOME/winner.so"
+wget https://github.com/jeremy302/alx-low_level_programming/raw/main/0x18-dynamic_libraries/101-winner.so -O "$HOME/winner.so"
 export LD_PRELOAD="$HOME/winner.so"
