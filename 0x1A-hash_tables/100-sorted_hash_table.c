c03a62d14e31fc5c32b34062a70bcc404b4c6975
jeremy302 <olajeremy123@gmail.com>
2021-10-08 04:54:45 +0100

    fixed a typo

--- 0x1A-hash_tables/100-sorted_hash_table.c
+++ 0x1A-hash_tables/100-sorted_hash_table.c
@@ -11,7 +11,7 @@
  */
 shash_table_t *shash_table_create(unsigned long int size)
 {
-	shash_table_t *ht = calloc(1, sizeof(hash_table_t) +
+	shash_table_t *ht = calloc(1, sizeof(shash_table_t) +
 							   size * sizeof(shash_node_t *));
 	if (ht != NULL)
 	{
